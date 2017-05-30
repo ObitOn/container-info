41d98e8af548d7cc78d053a5662ac9bb02492f4d
ObitOn <tibo.bries@telenet.be>
2017-05-30 11:37:50 +0200

    changed color

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 <!-- -------------------------- -->
 <!-- !!! CHANGE COLOR BELOW !!! -->
 
-<div class="card purple">
+<div class="red">
 
 <!-- !!! CHANGE COLOR ABOVE !!! -->
 <!-- -------------------------- -->
